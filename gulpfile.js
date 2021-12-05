1de1370964b6b775683cf19ce55a5b295acaf920
vsulimovvv <super.sulimov2014@yandex.ru>
2021-12-05 14:01:18 +0300

    начало проекта

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ function svgSprites() {
 
 
 function fileInclude() {
-  return src('app/html/**/*.html')
+  return src('app/html/*.html')
     .pipe(include({
       prefix: '@',
       basepath: '@file',
@@ -131,9 +131,9 @@ function cleanDist() {
 
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/html/**/*.html'], fileInclude);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
   watch(['app/images/icons/*.svg'], svgSprites);
 }
 
