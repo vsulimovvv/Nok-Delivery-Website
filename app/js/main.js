d494ae6b0d3a7acb80e76ac3a30efcf3deffb2ef
vsulimovvv <super.sulimov2014@yandex.ru>
2021-12-06 01:04:01 +0300

    остался адаптив

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".categories__slider", {
+  slidesPerView: 'auto',
+  slidesPerView: 4,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
