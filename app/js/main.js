013c281f3ce022067a9522a7245e663ae5f8531f
vsulimovvv <super.sulimov2014@yandex.ru>
2021-12-07 21:23:35 +0300

    добавил правки

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,54 @@
 const swiper = new Swiper(".categories__slider", {
-  slidesPerView: 'auto',
   slidesPerView: 4,
   spaceBetween: 20,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    991: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    }
+  }
 });
+
+const showMenu = () => {
+  const menuBtn = document.querySelector('.header__toggle');
+  const menu = document.querySelector('.menu');
+  const body = document.querySelector('body');
+  const overlay = document.querySelector('.overlay');
+
+  menuBtn.addEventListener('click', e => {
+    menu.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+    // overlay.classList.toggle('active');
+    // body.classList.toggle('no-scroll');
+  });
+
+
+
+  // overlay.addEventListener('click', e => {
+  //   menu.classList.remove('active');
+  //   overlay.classList.remove('active');
+  //   body.classList.remove('no-scroll');
+  //   menuBtn.classList.remove('active');
+  // });
+
+  // overlay.addEventListener('click', e => {
+  // menu.classList.remove('active');
+  // menuBtn.classList.remove('active');
+  // overlay.classList.remove('active');
+  // body.classList.remove('no-scroll');
+  // });
+
+};
+showMenu();
